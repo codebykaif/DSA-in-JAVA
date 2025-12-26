3efffbfc4518969de7e929262fb24108763acbcf
Mo kaif <kaifmd0808@gmail.com>
2025-12-26 08:06:12 +0530

    Update Encapsulation.java

--- OOPs/Encapsulation/Encapsulation.java
+++ OOPs/Encapsulation/Encapsulation.java
@@ -1,28 +1,27 @@
  import java.util.*;
  import java.util.Scanner; 
 
-// Student Marks (Basic Encapsulation)
+Student Marks (Basic Encapsulation)
+ class Student{
+     private int marks;
 
-// class Student{
-//     private int marks;
+    public void setMarks(int m){
+         if(m >= 0 && m <=  100){
+            marks = m;
+        }
+    }
+      public int getMarks(){
+         return marks;
+     }
+ }
+ public class Encapsulation {
 
-//     public void setMarks(int m){
-//         if(m >= 0 && m <=  100){
-//             marks = m;
-//         }
-//     }
-//     public int getMarks(){
-//         return marks;
-//     }
-// }
-// public class Encapsulation {
-
-//     public static void main(String[] args){
-//         Student s = new Student();
-//         s.setMarks(87);
-//         System.out.println(s.getMarks());
-//     }
-// }
+     public static void main(String[] args){
+         Student s = new Student();
+         s.setMarks(87);
+         System.out.println(s.getMarks());
+     }
+ }
 
 // Bank with Condition
 
